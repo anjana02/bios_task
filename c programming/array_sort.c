8319a533eddccb4e1faad9817de023cc51147f7d
Ash <75060827+anjana02@users.noreply.github.com>
2021-05-31 01:05:53 +0530

    Create array_sort.c

---
+++ c programming/array_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+   int a[100], i, n, j, t;
+   scanf("%d", &n);
+   for (i = 0; i < n; i++)
+   {
+      scanf("%d", a + i);
+   }
+      for (i = 0; i < (n - 1); i++)
+      {
+         for (j = i + 1; j < n; j++)
+         {
+            if (*(a + i) > *(a + j))
+            {
+               t = *(a + i);
+               *(a + i) = *(a + j);
+               *(a + j) = t;
+            }
+         }
+      }
+   for (i = 0; i < n; i++)
+   {
+      printf("%d ", *(a + i));
+   }
+   printf("\n");
+}
