be7a6dd7b5705ac4a54c26d41917f6baaf5b3d51
Ash <75060827+anjana02@users.noreply.github.com>
2021-05-31 11:04:15 +0530

    Update Toget10.c

--- c programming/Toget10.c
+++ c programming/Toget10.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{ int i,f,sum=0;
-scanf("%d",&i);
-f=i;
- int a[i];
-  for (;i>0;i--)
-  {
-      scanf("%d",&a[i]);
-      sum+=a[i];
-  }  
-  printf("%d", 19*f-2*sum);
+{
+    int i, f, sum = 0;
+    scanf("%d", &i);
+    f = i;
+    int a[i];
+    for (; i > 0; i--)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("%d", 19 * f - 2 * sum);
     return 0;
 }
